3cbee256071497074ddb1bb8d7349a60307bf99b
unknown <ddeickhardt@gmail.com>
2018-01-02 18:35:56 +0100

    First to stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { INgxMyDpOptions, IMyDateModel } from 'ngx-mydatepicker';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  category = '';
+  subCategory = '';
+  entryName = '';
+  dateOptions: INgxMyDpOptions = {
+    dateFormat: 'dd-mm-yyyy',
+    minYear: 2000,
+    maxYear: 2200
+  };
+  // Initialized to specific date (09.10.2018)
+  dateModel: any = { jsdate: new Date() };
+
+  // optional date changed callback
+  onDateChanged(event: IMyDateModel): void {
+    // date selected
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { NgxMyDatePickerModule } from 'ngx-mydatepicker';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgxMyDatePickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
